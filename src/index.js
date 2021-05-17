59f5d674af51104657d9cc72a146d093a578a11a
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-17 13:22:27 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const axios = require("axios");
+const { response } = require("express");
+app.use(express.json())
+const port = 5000
 
-app.get('/cliente', (req, res) => res.send('Hello World!'))
+const consultaClientes = {};
 
+const funcoes = {
+    clienteCriado: (clientes) => {
+        //Cliente Criado recebe as informações de cadastro apartir do id selecionado.
+        consultaClientes[clientes.id] = clientes;
+    },
+    ingressosCriado: (ingresso) => {
+    const ingressos = consultaClientes[ingresso.ingressoId]["ingressos"] || [];
+        ingressos.push(ingresso);
+        consultaClientes[ingresso.ingressoId]["ingressos"] = ingressos;
+    },
+    ClienteAtualizado: (cliente) => {
+        const clientes = baseConsulta[cliente]["clientes"];
+        const indice = clientes.findIndex((o) => o.id === cliente.id);
+        clientes[indice] = cliente;
+      },
+};
 
+app.get('/clientes', (req, res) => {
+    res.status(200).send(consultaClientes);
+})
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.post('/eventos', (req, res) => {
+    try {
+    funcoes[req.body.tipo](req.body.dados);
+    } catch (err){}
+        res.status(200).send(consultaClientes)
+})
+
+app.listen(port, async () => {
+  console.log("Consultas. Porta 6000");
+  const res = await axios.get("http://localhost:10000.eventos");
+
+  response.data.forEach((valor, indice, colecao) => {
+    try {
+      funcoes[valor.tipo](valor.dados);
+    } catch (err) {}
+  });
+});
